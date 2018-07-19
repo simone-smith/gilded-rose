59c753c0ee9365ed420f405e8f9060015a628c62
Simone <simonecaplin@gmail.com>
2018-07-19 17:17:22 +0100

    Extract increment and decrement quality methods

--- ruby/lib/gilded_rose.rb
+++ ruby/lib/gilded_rose.rb
@@ -10,48 +10,56 @@ class GildedRose
     item.sell_in -= 1
   end
 
+  def decrement_quality(item)
+    item.quality -= 1
+  end
+
+  def increment_quality(item)
+    item.quality += 1
+  end
+
   def update_quality()
     @items.each do |item|
       if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
         if item.quality > 0
           if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
+            decrement_quality(item)
           end
         end
       else
         if item.quality < 50
-          item.quality = item.quality + 1
+          increment_quality(item)
           if item.name == "Backstage passes to a TAFKAL80ETC concert"
             if item.sell_in < 11
               if item.quality < 50
-                item.quality = item.quality + 1
+                increment_quality(item)
               end
             end
             if item.sell_in < 6
               if item.quality < 50
-                item.quality = item.quality + 1
+                increment_quality(item)
               end
             end
           end
         end
       end
       if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
+        decrement_sell_in(item)
       end
       if item.sell_in < 0
         if item.name != "Aged Brie"
           if item.name != "Backstage passes to a TAFKAL80ETC concert"
             if item.quality > 0
               if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
+                decrement_quality(item)
               end
             end
           else
-            item.quality = item.quality - item.quality
+            item.quality = 0
           end
         else
           if item.quality < 50
-            item.quality = item.quality + 1
+            increment_quality(item)
           end
         end
       end
