221b30b8137dd4a6b48b64b3cd41e5bca68220b5
Simone <simonecaplin@gmail.com>
2018-07-22 10:46:14 +0100

    Refactor update method in RegularItem class

--- ruby/lib/backstage_passes.rb
+++ ruby/lib/backstage_passes.rb
@@ -6,20 +6,19 @@ class BackstagePasses
     @item = item
   end
 
-# MAKE SURE THE QUALITY CAN'T GO ABOVE 50!!
   def update
-      if @item.sell_in < 1
-        @item.quality = 0
-      elsif @item.sell_in <= 5
-        @item.quality += 3
-      elsif @item.sell_in <= 10
-        @item.quality += 2
-      else
-        @item.quality += 1
-      end
-      if @item.quality > 50
-        @item.quality = 50
-      end
+    if @item.sell_in < 1
+      @item.quality = 0
+    elsif @item.sell_in <= 5
+      @item.quality += 3
+    elsif @item.sell_in <= 10
+      @item.quality += 2
+    else
+      @item.quality += 1
+    end
+    if @item.quality > 50
+      @item.quality = 50
+    end
     @item.sell_in -= 1
   end
 end
--- ruby/lib/regular_item.rb
+++ ruby/lib/regular_item.rb
@@ -4,15 +4,17 @@ class RegularItem
   end
 
   def update
-    if @item.sell_in > 0
-      unless @item.quality == 0
-        @item.quality -= 1
-      end
-      @item.sell_in -= 1
-    else
-      unless @item.quality <= 1
-        @item.quality -= 2
-      end
-    end
+    update_quality
+    update_sell_in
   end
+
+  private
+    def update_quality
+      @item.sell_in > 0 ? @item.quality -= 1 : @item.quality -= 2
+      @item.quality = 0 if @item.quality < 0
+    end
+
+    def update_sell_in
+      @item.sell_in -= 1
+    end
 end
