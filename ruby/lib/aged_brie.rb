61ecb2923aafce396031558175865c911c7c5f12
Simone <simonecaplin@gmail.com>
2018-07-22 10:48:08 +0100

    Refactor update method in AgedBrie class

--- ruby/lib/aged_brie.rb
+++ ruby/lib/aged_brie.rb
@@ -5,13 +5,17 @@ class AgedBrie
   end
 
   def update
-    unless @item.quality == 50
-      if @item.sell_in > 0
-        @item.quality += 1
-      else
-        @item.quality += 2
-      end
-    end
-    @item.sell_in -= 1
+    update_quality
+    update_sell_in
   end
+
+  private
+    def update_quality
+      @item.sell_in > 0 ? @item.quality += 1 : @item.quality += 2
+      @item.quality = 50 if @item.quality > 50
+    end
+
+    def update_sell_in
+      @item.sell_in -= 1
+    end
 end
